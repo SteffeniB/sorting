adf42f7548d17d4001471c7c3b664b612e91ae44
Steffeni Bunsick <steffenibunsick@gmail.com>
2019-01-14 15:41:25 -0600

    split and merge working

--- mergesort.js
+++ mergesort.js
@@ -9,16 +9,50 @@ split([1, 2, 3]);
 
 function merge(arrOne, arrTwo) {
   let newArr = [];
-
-  for (let i = 0; i < arrOne.length; i++) {
-    if (arrOne[i] < arrTwo[i]) {
-      newArr.push(arrOne[i]);
+  // let mergedArr = arrOne.concat(arrTwo);
+  // for (let i = 0; i < mergedArr.length; i++) {
+  //   for (let j = 0; j < mergedArr.length; j++) {
+  //     let one = mergedArr[j];
+  //     let two = mergedArr[j - 1];
+  //     if (two < one) {
+  //       let temp = one;
+  //       one = two;
+  //       two = temp;
+  //       //arrOne = arrOne.slice(1);
+  //       newArr.push(two);
+  //     } else {
+  //       newArr.push(one);
+  //       //arrTwo = arrTwo.slice(1);
+  //     }
+  //   }
+  debugger;
+  let total = arrOne.length + arrTwo.length;
+  let indexLeft = 0;
+  let indexRight = 0;
+  while (newArr.length < total) {
+    let left = arrOne[indexLeft];
+    let right = arrTwo[indexRight];
+    if (left < right) {
+      newArr.push(left);
+      indexLeft++;
     } else {
-      newArr.push(arrTwo[i]);
+      newArr.push(right);
+      indexRight++;
+    }
+    if (indexLeft === arrOne.length) {
+      for (let i = indexRight; i < arrTwo.length; i++) {
+        newArr.push(arrTwo[i]);
+      }
+    }
+    if (indexRight === arrTwo.length) {
+      for (let i = indexLeft; i < arrOne.length; i++) {
+        newArr.push(arrOne[i]);
+      }
     }
   }
+
   console.log(newArr);
   return newArr;
 }
-
-merge([1, 2], [3, 4]);
+merge([2], [1]);
+merge([1, 3], [2, 4]);
